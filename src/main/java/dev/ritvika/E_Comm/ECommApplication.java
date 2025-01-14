613c1bc29ad08822064d0c6f0b7065efc8bc8631
RitvikaAN17 <ritvikasharma.1710@gmail.com>
2025-01-14 17:43:09 +0530

    Initial commit

---
+++ src/main/java/dev/ritvika/E_Comm/ECommApplication.java
@@ -0,0 +1,13 @@
+package dev.ritvika.E_Comm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommApplication.class, args);
+	}
+
+}
